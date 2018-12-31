fe3e34928a0fe7b337f1f79f411e3e0e25b47589
Xunner <161250042@smail.nju.edu.cn>
2018-12-31 16:58:57 +0800

    feature: rewrite home.html with Vue, ajax part left

--- j2ee_homework_front/src/components/HelloWorld.vue
+++ j2ee_homework_front/src/components/HelloWorld.vue
@@ -24,7 +24,12 @@
             <td>description</td>
             <td>number to buy</td>
           </tr>
-          $commodities
+          <tr v-for="commodity in commodities" :key="commodity.comId">
+            <td>{{commodity.name}}</td>
+            <td align="right">{{commodity.price}}</td>
+            <td>{{commodity.comment}}</td>
+            <td><label><input type="number" :name="commodity.comId" value=0></label></td>
+          </tr>
           <tr>
             <td colspan="4" align="center">
               <input type="submit" value="Add to cart">
@@ -44,7 +49,12 @@
             <td>number</td>
             <td>total (￥)</td>
           </tr>
-          $cart
+          <tr v-for="commodity in cart" :key="commodity.comId">
+            <td>{{commodity.name}}</td>
+            <td align="right">{{commodity.unitPrice}}</td>
+            <td align="right">{{commodity.number}}</td>
+            <td align="right">{{commodity.number * commodity.unitPrice}}</td>
+          </tr>
           <tr>
             <td colspan="4" align="right">Total price: ￥{{totalPrice}}</td>
           </tr>
@@ -62,12 +72,32 @@
 <script>
 module.exports = {
   data: function () {
-    return {
+    let ret = {
       userId: '123',
       totalNumber: 1,
       loggedInNumber: 0,
       visitorNumber: 1,
-      totalPrice: 100
+      cart: [{comId: 1, name: 'Gujian3', unitPrice: 99.0, number: 2},
+        {comId: 2, name: 'Overcooked', unitPrice: 69.0, number: 4}],
+      commodities: [{comId: 1, name: 'Gujian3', price: 99.0, comment: 'Hope of Chinese game'},
+        {comId: 2, name: 'Overcooked', price: 69.0, comment: 'Best multi-player games this year'}]
+    }
+    this.axios.get('/home.data')
+      .then(function (resp) {
+        console.log('Success!')
+        console.log(resp)
+        ret = resp
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    return ret
+  },
+  computed: {
+    totalPrice () {
+      return this.cart.reduce((sum, commodity) => {
+        return sum + commodity.number * commodity.unitPrice
+      }, 0)
     }
   }
 }
@@ -80,6 +110,6 @@ module.exports = {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #66ccff;
-    margin-top: 60px;
+    margin-top: 5px;
   }
 </style>
